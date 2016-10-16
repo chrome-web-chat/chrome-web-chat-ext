87b081398de5a696af8c1d3a77e08e27fd38efbd
Seth Rubin <seth.h.rubin@gmail.com>
2016-10-16 16:59:28 -0400

    google id

--- js/background.js
+++ js/background.js
@@ -1,3 +1,11 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.sendMessage(tab.id, {action: 'TOGGLE_CHAT'}, function(response) {});
 });
+
+chrome.identity.getAuthToken({ 'interactive': true }, function (token) {
+  console.log(token);
+  chrome.identity.getProfileUserInfo(function (userInfo) {
+    console.log(userInfo);
+    chrome.storage.sync.set({ id: userInfo.id });
+  });
+});
--- js/content.js
+++ js/content.js
@@ -1,27 +1,33 @@
-var url = encodeURIComponent(document.location.href);
-var serverUrl = 'https://chrome-web-chat.herokuapp.com';
-var socket = io.connect(serverUrl, { query: 'url=' + url });
+chrome.storage.sync.get('id', function(items) {
+  var id = items.id;
+  if (!id) {
+    console.log('id not found');
+    return;
+  }
+  var url = encodeURIComponent(document.location.href);
+  var serverUrl = 'https://chrome-web-chat.herokuapp.com';
+  var socket = io.connect(serverUrl, { query: 'url=' + url + '&id=' + id });
 
-$('body').append('<div id="cwc-foo"></div>');
+  $('body').append('<div id="cwc-foo"></div>');
 
-$('#cwc-foo').load(chrome.extension.getURL('content.html'), function() {
-  $('#cwc-form').submit(function(){
-    var obj = { name: $('#cwc-name').val(), msg: $('#cwc-msg').val() };
-    socket.emit('chat message', obj);
+  $('#cwc-foo').load(chrome.extension.getURL('content.html'), function() {
+    $('#cwc-form').submit(function(){
+      var obj = { name: $('#cwc-name').val(), msg: $('#cwc-msg').val() };
+      socket.emit('chat message', obj);
+      $('#cwc-messages').append($('<li>').text(obj.name + ': ' + obj.msg));
+      $('#cwc-msg').val('');
+      return false;
+    });
+  });
+
+  chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
+    if (msg.action == 'TOGGLE_CHAT') {
+      $('#cwc-foo').toggle("slow", function(){});
+    }
+  });
+
+  socket.on('chat message', function(obj){
     $('#cwc-messages').append($('<li>').text(obj.name + ': ' + obj.msg));
-    $('#cwc-msg').val('');
-    return false;
+    console.log(obj.name + ': ' + obj.msg);
   });
 });
-
-chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
-  if (msg.action == 'TOGGLE_CHAT') {
-    $('#cwc-foo').toggle("slow", function(){});
-  }
-});
-
-socket.on('chat message', function(obj){
-  $('#cwc-messages').append($('<li>').text(obj.name + ': ' + obj.msg));
-  console.log(obj.name + ': ' + obj.msg);
-});
-
