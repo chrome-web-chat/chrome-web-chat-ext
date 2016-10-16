f1ff0c3fbd69ea59cf1ade318939aae48768b2a9
Sahil Jain <sahil@sahiljain.ca>
2016-10-16 17:28:13 -0400

    cleanup ui a bit

--- js/content.js
+++ js/content.js
@@ -2,13 +2,13 @@ var url = encodeURIComponent(document.location.href);
 var serverUrl = 'https://chrome-web-chat.herokuapp.com';
 var socket = io.connect(serverUrl, { query: 'url=' + url });
 
-$('body').append('<div id="cwc-foo"></div>');
+$('body').append('<div id="cwc-container"></div>');
 
-$('#cwc-foo').load(chrome.extension.getURL('content.html'), function() {
+$('#cwc-container').load(chrome.extension.getURL('content.html'), function() {
   $('#cwc-form').submit(function(){
     var obj = { name: $('#cwc-name').val(), msg: $('#cwc-msg').val() };
     socket.emit('chat message', obj);
-    $('#cwc-messages').append($('<li>').text(obj.name + ': ' + obj.msg));
+    $('#cwc-message-list').append('<li class="cwc-message-sent"><div class="my-name">' + obj.name + '</div><div class="cwc-bubble">' + obj.msg + '</div></li>');
     $('#cwc-msg').val('');
     return false;
   });
@@ -16,12 +16,12 @@ $('#cwc-foo').load(chrome.extension.getURL('content.html'), function() {
 
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
   if (msg.action == 'TOGGLE_CHAT') {
-    $('#cwc-foo').toggle("slow", function(){});
+    $('#cwc-container').toggle("slow", function(){});
   }
 });
 
 socket.on('chat message', function(obj){
-  $('#cwc-messages').append($('<li>').text(obj.name + ': ' + obj.msg));
+  $('#cwc-message-list').append('<li class="cwc-message-received"><div class="their-name">' + obj.name + '</div><div class="cwc-bubble">' + obj.msg + '</div></li>');
   console.log(obj.name + ': ' + obj.msg);
 });
 
