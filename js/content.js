eded4d1ab94e87eea32afaf59ebaea4922944ea0
VVCepheiA <oyevera@gmail.com>
2016-10-16 17:48:10 -0400

    variable name change to match database; add chat history

--- js/content.js
+++ js/content.js
@@ -1,3 +1,6 @@
+const CHAT_MESSAGE_ENENT = 'chat message';
+const CHAT_HISTORY_ENENT = 'chat history';
+
 chrome.storage.sync.get('id', function(items) {
   var id = items.id;
   if (!id) {
@@ -12,9 +15,9 @@ chrome.storage.sync.get('id', function(items) {
 
   $('#cwc-foo').load(chrome.extension.getURL('content.html'), function() {
     $('#cwc-form').submit(function(){
-      var obj = { name: $('#cwc-name').val(), msg: $('#cwc-msg').val() };
+      var obj = { username: $('#cwc-name').val(), content: $('#cwc-msg').val() };
       socket.emit('chat message', obj);
-      $('#cwc-messages').append($('<li>').text(obj.name + ': ' + obj.msg));
+      $('#cwc-messages').append($('<li>').text(obj.username + ': ' + obj.content));
       $('#cwc-msg').val('');
       return false;
     });
@@ -26,8 +29,17 @@ chrome.storage.sync.get('id', function(items) {
     }
   });
 
-  socket.on('chat message', function(obj){
-    $('#cwc-messages').append($('<li>').text(obj.name + ': ' + obj.msg));
-    console.log(obj.name + ': ' + obj.msg);
+  socket.on(CHAT_MESSAGE_ENENT, function(obj){
+    $('#cwc-messages').append($('<li>').text(obj.username + ': ' + obj.content));
+    console.log(obj.username + ': ' + obj.content);
   });
+
+  socket.on(CHAT_HISTORY_ENENT, function(messages){
+    for (var i = 0; i < messages.length; i++) {
+      var obj = messages[i];
+      $('#cwc-messages').append($('<li>').text(obj.username + ': ' + obj.content));
+      console.log('history: ' + obj.username + ': ' + obj.content);
+    }
+  });
+
 });
