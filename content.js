93aec1e2f866e0409a5aac3da4667bb1a546455a
Sahil Jain <sahil@sahiljain.ca>
2016-10-16 02:48:36 -0400

    basic chrome ext is working

--- content.js
+++ content.js
@@ -1,1 +1,15 @@
-console.log("url: " + document.location.href);
+var url = encodeURIComponent(document.location.href);
+console.log("url: " + url);
+var socket = io.connect('https://chrome-web-chat.herokuapp.com', { query: 'url=' + url });
+socket.on('chat message', function(obj){
+  $('#messages').append($('<li>').text(obj.name + ': ' + obj.msg));
+  console.log(obj.name + ': ' + obj.msg);
+});
+$("body").append('<div id="foo"><ul id="messages"></ul><form id="cwc-form" action=""><label for="name">Name</label><input id="name" autocomplete="off" /><label for="msg">Message</label><input id="msg" autocomplete="off" /><button>Send</button></form></div>');
+$('#cwc-form').submit(function(){
+  var obj = { name: $('#name').val(), msg: $('#msg').val() };
+  socket.emit('chat message', obj);
+  $('#messages').append($('<li>').text(obj.name + ': ' + obj.msg));
+  $('#msg').val('');
+  return false;
+});
