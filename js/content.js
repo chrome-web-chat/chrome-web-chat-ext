f201e6e412f80c4ffc96aa5fcd5ff6677e130c09
Seth Rubin <seth.h.rubin@gmail.com>
2016-10-16 21:13:35 -0400

    refactor, add scrolling

--- js/content.js
+++ js/content.js
@@ -1,8 +1,22 @@
-const CHAT_MESSAGE_ENENT = 'chat message';
-const CHAT_HISTORY_ENENT = 'chat history';
+const CHAT_MESSAGE_EVENT = 'chat message';
+const CHAT_HISTORY_EVENT = 'chat history';
+
+var uid;
+
+function scroll(time) {
+  $('#cwc-message-list').animate({scrollTop: $('#cwc-message-list').prop("scrollHeight")}, time);
+}
+
+function addMessage(obj) {
+  if (uid === obj.uid) {
+    $('#cwc-message-list').append('<li class="cwc-message-sent"><div class="my-name">' + obj.username + '</div><div class="cwc-bubble">' + obj.content + '</div></li>');
+  } else {
+    $('#cwc-message-list').append('<li class="cwc-message-received"><div class="their-name">' + obj.username + '</div><div class="cwc-bubble">' + obj.content + '</div></li>');
+  }
+}
 
 chrome.storage.sync.get('id', function(items) {
-  var uid = items.id;
+  uid = items.id;
   if (!uid) {
     console.log('uid not found');
     return;
@@ -17,9 +31,10 @@ chrome.storage.sync.get('id', function(items) {
     $('#cwc-form').submit(function(){
       var username = $('#cwc-name').val().length? $('#cwc-name').val() : "Anonymous";
       var content = $('#cwc-msg').val().length? $('#cwc-msg').val() : " ";
-      var obj = { username: username, content: content };
+      var obj = { username: username, content: content, uid: uid };
       socket.emit('chat message', obj);
-      $('#cwc-message-list').append('<li class="cwc-message-sent"><div class="my-name">' + obj.username + '</div><div class="cwc-bubble">' + obj.content + '</div></li>');
+      addMessage(obj);
+      scroll(500);
       $('#cwc-msg').val('');
       return false;
     });
@@ -28,24 +43,23 @@ chrome.storage.sync.get('id', function(items) {
   chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
     if (msg.action == 'TOGGLE_CHAT') {
       $('#cwc-container').toggle("slow", function(){});
+      scroll(0);
     }
   });
 
-  socket.on(CHAT_MESSAGE_ENENT, function(obj){
-    $('#cwc-message-list').append('<li class="cwc-message-received"><div class="their-name">' + obj.username + '</div><div class="cwc-bubble">' + obj.content + '</div></li>');
+  socket.on(CHAT_MESSAGE_EVENT, function(obj){
+    addMessage(obj);
+    scroll(500);
     console.log(obj.username + ': ' + obj.content);
   });
 
-  socket.on(CHAT_HISTORY_ENENT, function(messages){
+  socket.on(CHAT_HISTORY_EVENT, function(messages){
     for (var i = messages.length - 1; i >= 0; i--) {
       var obj = messages[i];
-      if (obj.uid !== uid){
-        $('#cwc-message-list').append('<li class="cwc-message-received"><div class="their-name">' + obj.username + '</div><div class="cwc-bubble">' + obj.content + '</div></li>');
-      }else{
-        $('#cwc-message-list').append('<li class="cwc-message-sent"><div class="my-name">' + obj.username + '</div><div class="cwc-bubble">' + obj.content + '</div></li>');
-      }
+      addMessage(obj);
       console.log(obj.username + ': ' + obj.content);
     }
+    scroll(0);
   });
 
 });
